605b4c96a2c6a2ef9fff5dbfda2391aa89d33506
Murlidhar Properties <Murlidharproperties08@gmail.com>
2025-12-11 15:40:26 +0530

    Base component added

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+// Components
+import ChequeDesigner from './components/ChequeDesigner.jsx'
+
+// Styles
+import './App.css'
+
+function App() {
+  return (
+    <div style={{ padding: 20 }}>
+      <h1>Cheque Designer</h1>
+      <ChequeDesigner />
+    </div>
+  )
+}
+
+export default App
